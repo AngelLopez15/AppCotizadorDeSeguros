7212430988a2a266898e1852d27f25f2bd0bb312
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-07-03 01:31:40 -0500

    agregue los styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const ContenedorHeader = styled.header`
+    background-color: #26c6da;
+    color: #ffffff;
+    font-weight: bold;
+    padding: 10px;
+`
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin:0;
+    font-family:'Slabo 27px', serif;
+    text-align:center;
+`
+
 
 export const Header = ({titulo}) => {
     return (
-        <header>
-            <h1>{titulo}</h1>
-        </header>
+        <ContenedorHeader>
+            <TextoHeader>{titulo}</TextoHeader>
+        </ContenedorHeader>
     )
 }
