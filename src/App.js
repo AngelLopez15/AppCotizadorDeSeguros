02a188806350375db1aaf07d8ad3c6489eed36fa
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-07-05 00:12:25 -0500

    agregue componenete Formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled from '@emotion/styled'
+
 import {Header} from './components/Header'
+import {Formulario} from './components/Formulario'
+
+const Contenedor = styled.div`
+  max-width:600px;
+  margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+  background-color:#ffffff;
+  padding: 3rem;
+`
 
 function App() {
   return (
-    <Header 
-      titulo='Cotizador de seguros'
-    />
+    <Contenedor>
+      <Header 
+        titulo='Cotizador de seguros'
+      />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
