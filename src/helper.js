3607d7b111fc426aec59730cba06c61daa96b2c4
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-07-06 13:44:31 -0500

    agregue funcion para calcular aumento por marca

--- src/helper.js
+++ src/helper.js
@@ -2,3 +2,22 @@
 export const obtenerDiferenciaYear= (year) => {
     return new Date().getFullYear()-year
 }
+
+// toral a pagar segun la marca
+export const calcularMarca = (marca) =>{
+    let incremento;
+    switch (marca) {
+        case 'europeo':
+            incremento=1.30
+            break;
+        case 'americano':
+            incremento=1.15
+            break
+        case 'asiatico':
+            incremento=1.05
+            break;
+        default:
+            break;
+    }
+    return incremento
+}
